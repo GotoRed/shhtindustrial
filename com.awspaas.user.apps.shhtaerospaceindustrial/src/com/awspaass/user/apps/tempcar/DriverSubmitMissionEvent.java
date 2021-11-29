597b9377383e1c04ebbc8b3459245a577b1462a9
GotoRed <ssylszss@gmail.com>
2021-11-29 16:23:05 +0800

    New Feature: DriverSaveAction

--- com.awspaas.user.apps.shhtaerospaceindustrial/src/com/awspaass/user/apps/tempcar/DriverSubmitMissionEvent.java
+++ com.awspaas.user.apps.shhtaerospaceindustrial/src/com/awspaass/user/apps/tempcar/DriverSubmitMissionEvent.java
@@ -17,24 +17,38 @@ public class DriverSubmitMissionEvent extends ExecuteListener implements Execute
 	public void execute(ProcessExecutionContext pec) throws Exception {
 		try {
 			String bindId = pec.getProcessInstance().getId();//流程实例ID
-			String queryMissionSql = "SELECT * FROM BO_EU_SH_VEHICLEORDER_MISSION WHERE BINDID = '"+bindId+"'";
+			String queryMissionSql = "SELECT a.*, b.HONGQIAO,b.PUDONG,b.DAYPRICE,b.DAYOVERKILOMETERSPRICE,b.DAYOVERTIMEPRICE FROM BO_EU_SH_VEHICLEORDER_MISSION a ,BO_EU_SH_VEHICLETYPE b WHERE a.vehicletype=b.vehicletype AND a.vehiclelabelname=b.vehiclelabelname AND a.BINDID = '"+bindId+"'";
 			List<Map<String, Object>> missionList = DBSql.query(queryMissionSql, new ColumnMapRowMapper(), new Object[] {});
 			if(missionList != null && !missionList.isEmpty()) {
 				for (int i = 0; i < missionList.size(); i++) {
 					Map<String, Object> MissonInfo = missionList.get(i);
+					
+					double totalFee=0;
+					double totaloverTimeFee=0;
+					double totalOverKmFee=0;
+					double totalOverKm=0;
+					double totalOverTime=0;
+					double outFee=0;
+					int dayFee = 0;
+					double basicFee=(double)CoreUtil.objToInt(MissonInfo.get("DAYPRICE"));
+					double hongQiaoFee=(double)CoreUtil.objToInt(MissonInfo.get("HONGQIAO"));
+					double puDongFee=(double)CoreUtil.objToInt(MissonInfo.get("PUDONG"));
+					double overTimePrice = (double)CoreUtil.objToInt(MissonInfo.get("DAYOVERTIMEPRICE"));//超小时费(元/小时)
+					double overKmPrice = (double)CoreUtil.objToInt(MissonInfo.get("DAYOVERKILOMETERSPRICE"));//超公里费(元/公里)
+					
+					
 					String resourceTaskFpId = CoreUtil.objToStr(MissonInfo.get("RESOURCETASKFPID"));//来源任务分配单ID
 					String startTime = CoreUtil.objToStr(MissonInfo.get("CFSJ"));//出车时间
 					String getOnTime = CoreUtil.objToStr(MissonInfo.get("SKDATETIME"));//上客时间
 					String dropOffTime = CoreUtil.objToStr(MissonInfo.get("XKDATIME"));//下客时间
 					String retrunTime = CoreUtil.objToStr(MissonInfo.get("FHSJ"));//回场时间
+					String isOutShanghai = CoreUtil.objToStr(MissonInfo.get("ISOUTSHANGHAI"));
 					
-					
-					float overTimePrice = (float) CoreUtil.objToInt(MissonInfo.get("OTTIMEPRICE"));//超小时费(元/小时)
-					float overKmPrice = (float) CoreUtil.objToInt(MissonInfo.get("OTLEAVERICE"));//超公里费(元/公里)
-					float roadBridgeFee = (float) CoreUtil.objToInt(MissonInfo.get("GLF"));
-			        float hotelCost = (float) CoreUtil.objToInt(MissonInfo.get("STAYMONEY"));
-			        float parkingFee = (float) CoreUtil.objToInt(MissonInfo.get("TCF"));
-			        float otherFee = (float) CoreUtil.objToInt(MissonInfo.get("QTMONEY"));				
+
+					double roadBridgeFee = (double) CoreUtil.objToInt(MissonInfo.get("GLF"));
+					double hotelCost = (double) CoreUtil.objToInt(MissonInfo.get("STAYMONEY"));
+					double parkingFee = (double) CoreUtil.objToInt(MissonInfo.get("TCF"));
+					double otherFee = (double) CoreUtil.objToInt(MissonInfo.get("QTMONEY"));				
 					
 					int getOnDistance = CoreUtil.objToInt(MissonInfo.get("RESOURCETASKFPID"));//上客路码
 					int dropOffDistance = CoreUtil.objToInt(MissonInfo.get("RESOURCETASKFPID"));//下客路码
@@ -51,42 +65,45 @@ public class DriverSubmitMissionEvent extends ExecuteListener implements Execute
 					int useCarDistance = returnDistance-getOnDistance;
 					
 					if(billType.equals("2")) {
+						dayFee=(int)basicFee;
+						if("1".equals(isOutShanghai)) {
+							outFee= overTimePrice*5;
+						}
 						if(useCarTime <= 4){         
-			                totalBasicFee = Number(basicFee)/2;  
+							totalFee = basicFee/2;  
 			                if(useCarDistance>50){
-			                    $("#OVERERDISTANCENUM").val(Number(useCarDistance)-50);
-			                    $("#DAYOVERKILOMETERSPRICE").val(Number(useCarDistance-50)*Number(overKmFee));
-			                }
-			                totalOverKmFee = $("#DAYOVERKILOMETERSPRICE").val();                
+			                	totalOverKm = useCarDistance-50;
+			                	totalOverKmFee = (useCarDistance-50)*(overKmPrice);
+			                   
+			                }                
 			            }else if(useCarTime > 4){
-			                totalBasicFee = Number(basicFee); 
-			                if(Number(useCarDistance)>100){
-			                    $("#OVERERDISTANCENUM").val(Number(useCarDistance)-100);
-			                    $("#DAYOVERKILOMETERSPRICE").val(Number(useCarDistance-100)*Number(overKmFee));
+			            	totalFee = basicFee; 
+			                if(useCarDistance>100){
+			                	totalOverKm = useCarDistance-100;
+			                	totalOverKmFee = (useCarDistance-100)*(overKmPrice);
 			                }
-			                totalOverKmFee = $("#DAYOVERKILOMETERSPRICE").val();
 			                if(useCarTime > 9){
-			                    $("#OVERTIMENUM").val(useCarTime-9);
-			                    $("#DAYOVERTIMEPRICE").val(Number(useCarTime-9)*Number(overTimeFee));//超时费                     
+			                	totalOverTime = useCarTime-9;
+			                	totaloverTimeFee=totalOverTime*overTimePrice;
 			                }
-			                totalOverTimeFee = $("#DAYOVERTIMEPRICE").val();
 			            }
-			            totalFee = Number(totalBasicFee)+Number(totalOverKmFee)+Number(totalOverTimeFee)+Number(totalOtherFee);
-			            $("#TOTALMONEY").val(totalFee);
+			            totalFee = totalFee+outFee+totalOverKmFee+totaloverTimeFee+roadBridgeFee+hotelCost+parkingFee+otherFee;
+
 					}else if(billType.equals("1")||billType.equals("0")) {
-						
+						if(billType.equals("1")) {
+							dayFee=(int)(puDongFee);
+							totalFee=puDongFee;
+						}else if(billType.equals("0")) {
+							dayFee=(int)hongQiaoFee;
+							totalFee=(int)hongQiaoFee;
+						}
 					}
-
-					float basicFee = 0;//基础费用
-					float totalOverTime=0;//总超时时间
-					float totalOverTimeFee=0;
-					float totalOverKmFee=0;
-					
-					
+					String missionUpdateSql="UPDATE BO_EU_SH_VEHICLEORDER_MISSION SET QRLC= "+useCarDistance+",DAYPRICE= "+(int)dayFee+",DAYOVERKILOMETERSPRICE= "+totalOverKmFee+",DAYOVERTIMEPRICE= "+totaloverTimeFee+",USECARTIME="+useCarTime+",TOTALMONEY="+totalFee+
+							",OVERTIMENUM="+totalOverTime+",OVERERDISTANCENUM="+totalOverKm+",OTTIMEPRICE="+(int)overTimePrice+",OTLEAVERICE="+(int)overKmPrice+",CCGYF="+(int)outFee;
 					
 
-				        
-
+					DBSql.update(missionUpdateSql);
+					
 					DBSql.update("UPDATE BO_EU_SH_VEHICLEORDER_ASSIGMIS SET MISSIONSTATUS = '4' WHERE ID = '"+resourceTaskFpId+"'");					
 				}
 				
